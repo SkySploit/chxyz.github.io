77c1c2be67e10ed4c8dbece38059a6ced1578964
SkySploit <dayers402@gmail.com>
2025-02-15 11:01:57 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function showInfo(message) {
+    alert(message);
+}
+
+window.addEventListener('load', () => {
+    const music = document.getElementById('background-music');
+    if (music) {
+        music.play();
+    }
+});
